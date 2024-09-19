69b32c2ac45fa8e6a8c78a2f54a874e643eaeaaa
Michal Valo <michal.valo6@gmail.com>
2024-09-19 16:15:59 +0200

    Navbar fixes

--- partials/layout/navbar.php
+++ partials/layout/navbar.php
@@ -1,16 +1,16 @@
 <nav class="container my-16">
     <div class="px-24 py-16 flex justify-between items-center rounded-16 bg-neutral-800">
         <a href="/">
-            <h1 class="h3 font-semibold text-primary-300">Investio.</h1>
+            <h1 class="h4 md:h3 font-semibold text-primary-300">Investio.</h1>
         </a>
 
-        <ul class="space-x-32 flex items-center">
+        <ul class="space-x-24 md:space-x-32 flex items-center">
             <li>
-                <a class="font-semibold uppercase text-sm" href="/">Domov</a>
+                <a class="font-semibold uppercase text-xs md:text-sm" href="/">Domov</a>
             </li>
 
             <li>
-                <a class="font-semibold uppercase text-sm" href="https://github.com/myzo-dev" target="_blank">Source</a>
+                <a class="font-semibold uppercase text-xs md:text-sm" href="https://github.com/myzo-dev" target="_blank">Source</a>
             </li>
         </ul>
     </div>
